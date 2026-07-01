34daa89371c27c57b98bea2805fb22ae4eecd9ac
princetbzxx <princetbzxx@gmail.com>
2026-07-01 07:03:45 +0100

    new changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <script setup>
 import Footer from './components/layout/Footer.vue';
 import Navbar from './components/layout/Navbar.vue';
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+document.documentElement.setAttribute(
+  "data-theme",
+  prefersDark.matches ? "dark" : "light"
+);
+
+prefersDark.addEventListener("change", (e) => {
+  document.documentElement.setAttribute(
+    "data-theme",
+    e.matches ? "dark" : "light"
+  );
+});
 
 
 </script>
